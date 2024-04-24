c7f31d4f40605d356e6362e884a7d272ffe083ca
thiagotassinari1 <126289953+thiagotassinari1@users.noreply.github.com>
2024-04-24 12:25:28 -0300

    Delete back_api directory

--- back_api/src/routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const router = Router();
-
-const { CriarUsuario } = require('../controller/userController');
-
-router.post('/store/usuario', CriarUsuario);
-
-module.exports = router;
--- back_api/src/server.js
+++
@@ -1,4 +0,0 @@
-const app = require('./app');
-const port = app.get('port');
-
-app.listen(port, () => console.log(`Rodando na porta ${port}`));
