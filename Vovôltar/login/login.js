3e446de3871f0709d8738b8e6714046476c51dab
thiagotassinari1 <126289953+thiagotassinari1@users.noreply.github.com>
2024-07-08 12:23:11 -0300

    estrutura

--- Vovôltar/login/login.js
+++ Vovôltar/login/login.js
@@ -6,21 +6,26 @@ logarButton.onclick = async function (e) {
     let email = document.getElementById('email').value;
     let senha = document.getElementById('senha').value;
 
-    let data = { email, senha };
-
-    const response = await fetch('http://localhost:3001/api/post/login', {
-        method: "POST",
-        headers: { "Content-type": "application/json;charset=UTF-8" },
-        body: JSON.stringify(data)
-    });
-
-    let content = await response.json();
-    console.log(content);
-
-    if (content.success) {
-        alert('Sucesso no login!');
-        window.location.href = '../home/home.html'
+    if (!email || !senha ) {
+        alert('Preencha todos os campos!')
+        return false
     } else {
-        alert('Erro no login, tente novamente!');
+        let data = { email, senha };
+
+        const response = await fetch('http://localhost:3001/api/post/login', {
+            method: "POST",
+            headers: { "Content-type": "application/json;charset=UTF-8" },
+            body: JSON.stringify(data)
+        });
+
+        let content = await response.json();
+        console.log(content);
+
+        if (content.success) {
+            alert('Sucesso no login!');
+            window.location.href = '../home/home.html'
+        } else {
+            alert('Erro no login, tente novamente!');
+        }
     }
 };
